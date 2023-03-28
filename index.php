16c92146085ec66913ae694f9f1571b10884b1c7
sergionunezmeneses <snunezmeneses@gmail.com>
2023-03-28 18:17:58 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="public/css/style.css">
+	<script type="module" src="public/js/script.js"></script>
+	<title></title>
+</head>
+<body></body>
+</html>
