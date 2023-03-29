4a2858df88ac8a548244409bdefbb1dc65591346
sergionunezmeneses <snunezmeneses@gmail.com>
2023-03-29 15:40:27 +0200

    Add .htaccess & Move index.php to public directory

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
-<?php require "src/AutoLoader.php"; ?>
+<?php require "../src/AutoLoader.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="public/css/style.css">
-	<script type="module" src="public/js/script.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="module" src="js/script.js"></script>
 	<title></title>
 </head>
 <body></body>
