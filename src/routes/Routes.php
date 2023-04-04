32acc90170436bbe03bc565285fd1f029e58266f
sergionunezmeneses <snunezmeneses@gmail.com>
2023-04-04 19:07:33 +0200

    Update Routes.php

--- src/routes/Routes.php
+++ src/routes/Routes.php
@@ -6,16 +6,16 @@ class Routes {
 
 	private array $routes = [
 			[
+					"name"              => "ajax",
+					"path"              => "/ajax",
+					"controller"        => "AjaxController",
+					"controller_method" => "index",
+					"methods"           => ["post"],
+			],
+			[
 					"name"              => "index",
 					"path"              => '/',
 					"controller"        => "IndexController",
-					"controller_method" => "index",
-					"methods"           => ["get"],
-			],
-			[
-					"name"              => "search_index",
-					"path"              => "/search",
-					"controller"        => "SearchController",
 					"controller_method" => "index",
 					"methods"           => ["get"],
 			],
